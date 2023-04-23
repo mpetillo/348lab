18884150f6a32d07a9d50303075dbce5d1de1d44
Matthew <mpetillo@ku.edu>
2023-04-23 15:47:37 -0500

    pushing lab 9

---
+++ Lab 9/webapp/functions.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+    <style>
+        table, tr, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+<table>
+    <tr>
+        <td></td>
+        <?php for ($i = 1; $i <= $_POST["size"]; $i++){
+            echo "<td>$i</td>";}
+        ?>
+    </tr>
+    <?php for ($i = 1; $i <= $_POST["size"]; $i++){
+        echo "<tr>";
+        echo "<td>" . $i . "</td>";
+        for ($j = 1; $j <= $_POST["size"]; $j++){
+            echo "<td>" . $i*$j . "</td>";
+        }
+        echo "</tr>";
+    }?>
+</table>
+</body>
+</html> 
